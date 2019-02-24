d695c797c3e4342adac5963dad7210d82cb0d193
Christine Dodrill <me@christine.website>
2019-02-24 13:24:31 -0800

    remove trace

--- cmd/bsnk/main.go
+++ cmd/bsnk/main.go
@@ -4,13 +4,10 @@ import (
 	"context"
 	"flag"
 	"net/http"
-	"path/filepath"
-	"time"
 
 	"github.com/Xe/bsnk/api"
 	"github.com/Xe/bsnk/snakes"
 	"github.com/facebookgo/flagenv"
-	"golang.org/x/net/trace"
 	"within.website/ln"
 	"within.website/ln/ex"
 	"within.website/ln/opname"
@@ -21,31 +18,10 @@ func index(res http.ResponseWriter, req *http.Request) {
 	res.Write([]byte("Battlesnake documentation can be found at <a href=\"https://docs.battlesnake.io\">https://docs.battlesnake.io</a>."))
 }
 
-func middlewareSpan(family string, next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		sp := trace.New(family, filepath.Base(r.URL.Path))
-		defer sp.Finish()
-		ctx, cancel := context.WithTimeout(r.Context(), 30*time.Second)
-		defer cancel()
-
-		ctx = trace.NewContext(ctx, sp)
-
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
-}
-
 var (
 	port   = flag.String("port", "5000", "http port to listen on")
 	gitRev = flag.String("git-rev", "", "if set, use this git revision for the color code")
 )
-
-func init() {
-	ln.AddFilter(ex.NewGoTraceLogger())
-
-	trace.AuthRequest = func(_ *http.Request) (bool, bool) {
-		return true, true
-	}
-}
 
 func main() {
 	flagenv.Parse()
@@ -54,7 +30,7 @@ func main() {
 	ctx := opname.With(context.Background(), "main")
 
 	http.HandleFunc("/", index)
-	http.Handle("/garen/", middlewareSpan("garen", api.Server{Brain: snakes.Garen{}}))
+	http.Handle("/garen/", api.Server{Brain: snakes.Garen{}})
 
 	ln.Log(ctx, ln.Info("booting"))
 	ln.FatalErr(ctx, http.ListenAndServe(":"+*port, ex.HTTPLog(http.DefaultServeMux)))

dc532cd7e233958dab3a05f35f55e399125bf999
Christine Dodrill <me@christine.website>
2019-02-24 10:15:04 +0000

    aaa

--- main.go
+++ main.go
@@ -251,13 +251,13 @@ func (b bot) move(res http.ResponseWriter, req *http.Request) {
 		if sk.ID != decoded.You.ID {
 			pt := sk.Body[0]
 			lf := pt.Left()
-			pf.SetAdditionalCost(lf.X, lf.Y, 2)
+			pf.SetAdditionalPointCost(lf.X, lf.Y, 2)
 			lf = pt.Right()
-			pf.SetAdditionalCost(lf.X, lf.Y, 2)
+			pf.SetAdditionalPointCost(lf.X, lf.Y, 2)
 			lf = pt.Up()
-			pf.SetAdditionalCost(lf.X, lf.Y, 2)
+			pf.SetAdditionalPointCost(lf.X, lf.Y, 2)
 			lf = pt.Down()
-			pf.SetAdditionalCost(lf.X, lf.Y, 2)
+			pf.SetAdditionalPointCost(lf.X, lf.Y, 2)
 		}
 	}
 

d40ddb434bd69544f8296491383a1d6d4fe7458d
Christine Dodrill <me@christine.website>
2019-02-27 17:50:41 -0800

    don't avoid things more

--- snakes/common.go
+++ snakes/common.go
@@ -17,15 +17,7 @@ func makePathfinder(decoded api.SnakeRequest) ([][]int, *goeasystar.Pathfinder) 
 		grid[i] = make([]int, decoded.Board.Width)
 
 		for j := range grid[i] {
-			if j == 0 || j == len(grid[i])-1 {
-				grid[i][j] = 8
-			}
-
-			if i == 0 || i == len(grid)-1 {
-				grid[i][j] = 8
-			} else {
-				grid[i][j] = 1
-			}
+			grid[i][j] = 1
 		}
 	}
 
